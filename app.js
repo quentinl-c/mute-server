141aeb061a841a392cab96dff003a52501c1fef6
quentinl-c <quentin.laportechabasse@linagora.com>
2016-06-09 15:23:27 +0200

    fix

--- app.js
+++ app.js
@@ -91,9 +91,9 @@ fs.readFile('mute.conf', 'utf8', function (err,data) {
         console.log('Connection to mongoDB instance succeed!');
     });
 
-    HOST_SIGNALING = process.env.SIGNALING_SERVER_HOST || 'localhost';
+    HOST_SIGNALING = process.env.OPENSHIFT_NODEJS_IP  || process.env.SIGNALING_SERVER_HOST || 'localhost';
     PORT_SIGNALING = process.env.SIGNALING_SERVER_PORT || 8000;
-	URI_SIGNALING = 'ws://'  + HOST_SIGNALING + ':' + PORT_SIGNALING;
+    URI_SIGNALING = 'ws://'  + HOST_SIGNALING + ':' + PORT_SIGNALING;
 
     console.log('Signaling server URI to be used: ' + URI_SIGNALING);
 });
@@ -128,7 +128,7 @@ app.set('views', __dirname + '/views');
 var server_session_id = createID();
 
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
-var port = 80;
+var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 
 
 var delay = 0;
